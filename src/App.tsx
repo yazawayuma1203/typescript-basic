82cb00b5ad17399f669900961ea10266ed10cd63
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-08-14 00:39:14 +0900

    返却値の型指定 返却値の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Practice1 } from "./placticies/Practice1";
+import { Practice2 } from "./placticies/Practice2";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Practice1 />
+      <Practice2 />
     </div>
   );
 }
